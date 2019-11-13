6ab5c30d27d3b9bb298fa950fda93125d0e18341
baconbits1204 <rafaelhermoso1204@gmail.com>
2019-11-12 23:39:49 -0500

    file tree, database, and server setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Big-Mac", false), ("Whopper", false), ("Big-Kahuna-Burger", false);
---
+++ server.js
@@ -0,0 +1,26 @@
+//pulling in requirements
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+//running express
+const app = express();
+//set up port to make deployment friendly for Heroku
+const PORT = process.env.PORT || 3000;
+
+//static files
+app.use(express.static("public"));
+//adding body-parser
+app.use(bodyParser.urlencoded({ extended: true}));
+//set up handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+//routes to build
+const routes = require("./controllers/burgers_controller.js/index.js");
+//use routes
+app.use(routes);
+//make server listen
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost" + PORT);
+});
